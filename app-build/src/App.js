2202656ea66a91e81b2a01694c31a7203d7414d1
Mustafa Farah <mustafa.farah@outlook.com>
2018-07-07 13:49:13 -0500

    adding navbar dummy links

--- app-build/src/App.js
+++ app-build/src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <Nav />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
--- app-build/src/components/Nav/Nav.js
+++ app-build/src/components/Nav/Nav.js
@@ -3,11 +3,22 @@ import "./Nav.css";
 
 const Nav = props => (
     <div>
-        <ul className="nav nav-bar nav-justified">
-            <li className= "title"><a href="/">XXXXXXXXXXXXXX</a></li>
+       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+         <a className="navbar-brand" href="/">Submit a Photo</a>
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span className="navbar-toggler-icon"></span>
+         </button>
 
-        </ul>
-    </div>
-);
+         <div>
+         <a className="navbar-brand" href="/">Log In / Create User</a>
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+           <span className="navbar-toggler-icon"></span>
+         </button>
+         </div>
+       </nav>
+   </div>
+);  
 
 export default Nav;
