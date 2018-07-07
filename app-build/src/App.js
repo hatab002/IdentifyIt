a323812b709302c28b43fa6ad58fcca36999f3d7
Mustafa Farah <mustafa.farah@outlook.com>
2018-07-07 17:17:54 -0500

    addding placeholders for different sections

--- app-build/src/App.js
+++ app-build/src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Wrapper from './components/Wrapper'
-import Nav from './components/Nav'
-import Card from "./components/Card";
-import Carousel from "./components/Carousel"
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
+import Nav from './components/Nav';
+import Card from './components/Card';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Nav />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Carousel />
+          <header>
+            <Nav />
+
+            <Header/>
+          </header>
+
+          <main className="App-main">
+            <Carousel />
+          </main>
+    
+          <footer className= "App-footer">
+            <Footer />
+          </footer>
+       
+
       </div>
     );
   }
