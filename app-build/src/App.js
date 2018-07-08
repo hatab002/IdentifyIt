a3bbd030156c4e41118a489d0a6490e3dfbf41bb
Adam Stephenson <astephensonx@gmail.com>
2018-07-08 01:07:18 -0500

    carousel functioning

--- app-build/src/App.js
+++ app-build/src/App.js
@@ -11,13 +11,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Carousel />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <Carousel />
+          </div>
+        </div>
+      </div>
       </div>
     );
   }
--- app-build/src/components/Carousel/Carousel.js
+++ app-build/src/components/Carousel/Carousel.js
@@ -3,27 +3,70 @@ import "./Carousel.css";
 
 
 const Carousel = () => (
-    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-    <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img className="d-block w-100" src="..." alt="First slide"></img>
+
+    <div id="imageCarousel" className="carousel slide multi-item-carousel" data-interval="3000" 
+         data-ride="carousel" data-pause="hover">
+
+        <div className="carousel-inner">
+            <div className="carousel-item active">
+                <div className="row">
+                    <div className="col-md-4">
+                        <img src="https://media.self.com/photos/5aeb2496a982843a568c9dab/4:3/w_728,c_limit/extreme-morning-person.jpg" 
+                             alt="First slide" className="image-responsive"></img>
+                    </div>
+                    <div className="col-md-4">
+                        <img src="https://s-i.huffpost.com/gen/1308835/images/o-FOX-facebook.jpg" 
+                             alt="Second slide" className="image-responsive"></img>
+                    </div>
+                    <div className="col-md-4">
+                        <img src="http://cdn3-www.mandatory.com/assets/mandatory/legacy/2016/02/man_file_1064762_1banana.jpg" 
+                             alt="Third slide" className="image-responsive"></img>
+                    </div>
+                </div>
+            </div>
+            <div className="carousel-item">
+                <div className="row">
+                <div className="col-md-4">
+                        <img src="https://media.self.com/photos/5aeb2496a982843a568c9dab/4:3/w_728,c_limit/extreme-morning-person.jpg" 
+                             alt="First slide" className="image-responsive"></img>
+                    </div>
+                    <div className="col-md-4">
+                        <img src="https://s-i.huffpost.com/gen/1308835/images/o-FOX-facebook.jpg" 
+                             alt="Second slide" className="image-responsive"></img>
+                    </div>
+                    <div className="col-md-4">
+                        <img src="http://cdn3-www.mandatory.com/assets/mandatory/legacy/2016/02/man_file_1064762_1banana.jpg" 
+                             alt="Third slide" className="image-responsive"></img>
+                    </div>
+                </div>
+            </div>
+            <div className="carousel-item">
+            <div className="row">
+            <div className="col-md-4">
+                        <img src="https://media.self.com/photos/5aeb2496a982843a568c9dab/4:3/w_728,c_limit/extreme-morning-person.jpg" 
+                             alt="First slide" className="image-responsive"></img>
+                    </div>
+                    <div className="col-md-4">
+                        <img src="https://s-i.huffpost.com/gen/1308835/images/o-FOX-facebook.jpg" 
+                             alt="Second slide" className="image-responsive"></img>
+                    </div>
+                    <div className="col-md-4">
+                        <img src="http://cdn3-www.mandatory.com/assets/mandatory/legacy/2016/02/man_file_1064762_1banana.jpg" 
+                             alt="Third slide" className="image-responsive"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+        </a>
+        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+        </a>
     </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src="..." alt="Second slide"></img>
-    </div>
-    <div className="carousel-item">
-      <img className="d-block w-100" src="..." alt="Third slide"></img>
-    </div>
-  </div>
-  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="sr-only">Previous</span>
-  </a>
-  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="sr-only">Next</span>
-  </a>
-</div>
 
 );
 
