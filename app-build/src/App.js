45116cddc5891212ad12cb88edb83f54c2300118
Adam Stephenson <astephensonx@gmail.com>
2018-07-08 23:13:43 -0500

    needed to remove reference to deleted svg image

--- app-build/src/App.js
+++ app-build/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import Wrapper from './components/Wrapper';
 import Nav from './components/Nav';
 import Card from './components/Card';
 import Carousel from './components/Carousel';
