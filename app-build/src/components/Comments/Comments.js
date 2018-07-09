e9a800e1248c673a71d09023500ec0757218f384
Adam Stephenson <astephensonx@gmail.com>
2018-07-08 23:10:23 -0500

    more styling and another mock-up

--- app-build/src/components/Comments/Comments.js
+++ app-build/src/components/Comments/Comments.js
@@ -1,5 +1,13 @@
 import React from "react";
 import "./Comments.css";
 
+const Comments = props => (
+    
+    <div className="comments-container">
+        Comments
+        <br />
+        bllha  lknclsdaks klasdhasudls a;lskchsdcmsa;/
+    </div>
+)
 
 export default Comments;
--- app-build/src/components/Nav/Nav.js
+++ app-build/src/components/Nav/Nav.js
@@ -3,15 +3,15 @@ import "./Nav.css";
 
 const Nav = props => (
     <div>
-       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-         <a className="navbar-brand" href="/">Submit a Photo</a>
+       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+         <a id="submit-picture" className="navbar-brand" href="/">Submit a Photo</a>
          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
            <span className="navbar-toggler-icon"></span>
          </button>
 
          <div>
-         <a className="navbar-brand"  href="/">Log In / Create User</a>
+         <a id="login" className="navbar-brand"  href="/">Log In / Create User</a>
          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
            <span className="navbar-toggler-icon"></span>
