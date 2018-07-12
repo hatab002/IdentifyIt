1cbfe0e22bdd5652cbd803c4cf9ef435fcafb28d
Adam Stephenson <astephensonx@gmail.com>
2018-07-11 23:38:37 -0500

    modified modals and added signed in/signed out condition

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,28 +3,33 @@ import Login from '../Login';
 import Submit from '../Submit';
 import "./Nav.css";
 
-const Nav = props => (
-    <div>
-       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-         <p id="submit-picture" className="navbar-brand" data-toggle="modal" data-target="#submitPhoto">Submit a Photo</p>
-         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-           <span className="navbar-toggler-icon"></span>
-         </button>
 
+const Nav = (props) => {
+
+  const isLoggedIn = props.isLoggedIn;
+
+  if (isLoggedIn) {
+
+    return <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <p id="submit-picture" className="navbar-brand" data-toggle="modal" data-target="#submitPhoto">Submit a Photo</p>
+        <Submit />
+        <div>
+          <p id="sign-out" className="navbar-brand" data-toggle="modal" data-target="#userLogin">Sign Out</p>
+        </div>
+      </nav>
+    </div>
+  }
+  return <div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <p id="submit-picture" className="navbar-brand" data-toggle="modal" data-target="#submitPhoto">Submit a Photo</p>
       <Submit />
-
-         <div>
-         <p id="login" className="navbar-brand"  data-toggle="modal" data-target="#userLogin">Log In / Create User</p>
-         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-           <span className="navbar-toggler-icon"></span>
-         </button>
-         </div>
-       </nav>
-
-       <Login />
-   </div>
-);  
+      <div>
+        <p id="login" className="navbar-brand" data-toggle="modal" data-target="#userLogin">Log In/Create User</p>
+      </div>
+    </nav>
+    <Login />
+  </div>
+};
 
 export default Nav;
--- client/src/components/Submit/Submit.js
+++ client/src/components/Submit/Submit.js
@@ -6,17 +6,17 @@ const Submit = () => (
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLongTitle">Submit a Photo!</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-          ...
+          <input type="file"/>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-primary">Submit</button>
         </div>
       </div>
     </div>
