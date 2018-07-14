15212a2bb30a7a8e9d7d0150446f75836f7666cf
Adam Stephenson <astephensonx@gmail.com>
2018-07-14 10:08:32 -0500

    login features done

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,27 +1,73 @@
 import React from "react";
 import "./Login.css";
 
-
 const Login = (props) => (
-    <div class="modal fade" id="userLogin" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div>
+    <div class="modal fade" id="createUser" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+          <h5 class="modal-title" id="exampleModalLongTitle">Create an Account</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-          ...
+        <form>
+          <div class="form-group">
+            <label for="create-account-email">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+          </div>
+          <div class="form-group">
+            <label for="create-account-password">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+          </div>
+          <button type="submit" class="btn btn-primary">Create Account</button>
+        </form>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
   </div>
+
+      <div class="modal fade" id="userLogin" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Login</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        <form>
+        <div class="form-group">
+            <label for="username">Username</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+          </div>
+          <div class="form-group">
+            <label for="login-email">Email address</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+          </div>
+          <div class="form-group">
+            <label for="login-password">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+          </div>
+          <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
+
 )
 
 export default Login;
