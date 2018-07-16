fe29a0e9ec11b1f7aa2601e6f49e10a195ce7b9a
Mustafa Farah <mustafa.farah@outlook.com>
2018-07-16 18:56:54 -0500

    fixed all carousel issues

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -2,64 +2,36 @@ import React from "react";
 import "./Carousel.css";
 
 
-const Carousel = (props) => (
-
-  
-    <div id="imageCarousel" className="carousel slide multi-item-carousel" data-interval="2000" 
+class Carousel extends React.Component {
+    componentDidMount() {
+        this.interval = setInterval(() => {
+            this.setState({start: this.state.start + 1})
+        }, 70000)
+    }
+    state = {
+        start: 0
+    }
+    shutter = (arr, start) => {
+        return [start, start + 1, start + 2]
+        .map(idx => arr[idx % arr.length])
+    }
+    render() {
+      const props = this.props
+      return <div id="imageCarousel" className="carousel slide multi-item-carousel" 
          data-ride="carousel" data-pause="hover">
  
         <div className="carousel-inner">
-            <div className="carousel-item">
+            <div className="carousel-item active">
                 <div className="row">
-                    {props.witts.map((witt, index) => (
-                        <div className="col-md-2">
+                    {this.shutter(props.witts, this.state.start).map((witt, index) => (
+                        <div 
+                        className="col-md-4">
                         <img src={witt.image} 
-                            alt="First slide" className="image-responsive" onClick={()=>props.updateCard(index)}></img>
+                            alt="First slide" className="image-responsive" onClick={()=>props.updateCard(witt.id -1)}></img>
                     </div>
                     ))}
-       
-                    {/* <div className="col-md-4">
-                        <img src="https://s-i.huffpost.com/gen/1308835/images/o-FOX-facebook.jpg" 
-                             alt="Second slide" className="image-responsive" onClick={props.updateCard}></img>
-                    </div>
-                    <div className="col-md-4">
-                        <img src="http://cdn3-www.mandatory.com/assets/mandatory/legacy/2016/02/man_file_1064762_1banana.jpg" 
-                             alt="Third slide" className="image-responsive" onClick={props.updateCard}></img>
-                    </div> */}
                 </div>
             </div>
-            {/* <div className="carousel-item">
-                <div className="row">
-                <div className="col-md-4">
-                        <img src="https://media.self.com/photos/5aeb2496a982843a568c9dab/4:3/w_728,c_limit/extreme-morning-person.jpg" 
-                             alt="First slide" className="image-responsive" onClick={props.updateCard}></img>
-                    </div>
-                    <div className="col-md-4">
-                        <img src="https://s-i.huffpost.com/gen/1308835/images/o-FOX-facebook.jpg" 
-                             alt="Second slide" className="image-responsive" onClick={props.updateCard}></img>
-                    </div>
-                    <div className="col-md-4">
-                        <img src="http://cdn3-www.mandatory.com/assets/mandatory/legacy/2016/02/man_file_1064762_1banana.jpg" 
-                             alt="Third slide" className="image-responsive" onClick={props.updateCard}></img>
-                    </div>
-                </div>
-            </div> */}
-            {/* <div className="carousel-item">
-            <div className="row">
-            <div className="col-md-4">
-                        <img src="https://media.self.com/photos/5aeb2496a982843a568c9dab/4:3/w_728,c_limit/extreme-morning-person.jpg" 
-                             alt="First slide" className="image-responsive"></img>
-                    </div>
-                    <div className="col-md-4">
-                        <img src="https://s-i.huffpost.com/gen/1308835/images/o-FOX-facebook.jpg" 
-                             alt="Second slide" className="image-responsive"></img>
-                    </div>
-                    <div className="col-md-4">
-                        <img src="http://cdn3-www.mandatory.com/assets/mandatory/legacy/2016/02/man_file_1064762_1banana.jpg" 
-                             alt="Third slide" className="image-responsive"></img>
-                    </div>
-                </div>
-            </div> */}
         </div>
 
         <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -71,7 +43,8 @@ const Carousel = (props) => (
             <span className="sr-only">Next</span>
         </a>
     </div>
+    }
 
-);
+};
 
 export default Carousel;
